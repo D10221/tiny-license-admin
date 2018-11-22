f50c10084d631ab45f52323279cd6bef7e4f1f66
Daniel <danieliozzi@gmail.com>
2018-11-22 07:16:32 +0200

    cli/cmds

--- packages/tiny-license-cmds/src/index.js
+++ packages/tiny-license-cmds/src/index.js
@@ -1,1 +1,19 @@
-// TODO
+import minimist from "minimist";
+const argv = minimist(process.argv.slice(2));
+
+import validateRemote from "./validate-remote";
+
+const { validate, ...args } = argv;
+
+function run() {
+  if (validate)
+    return validateRemote(args).then(x => {
+      console.log("Result: %s", x);
+    });
+  return Promise.reject(new Error("Not Implemented"));
+}
+
+run().catch(error => {
+  console.error(error);
+  process.exit(-1);
+});
--- packages/tiny-license-cmds/src/validate-remote.js
+++ packages/tiny-license-cmds/src/validate-remote.js
@@ -1,63 +1,90 @@
 import fetch from "node-fetch";
-import minimist from "minimist";
 import { readFileSync } from "fs";
-const args = minimist(process.argv.slice(2));
+import jwt from "jsonwebtoken";
 /**
- * @param {string} token
- * @return {string} 
+ *
+ * @param {import("minimist").ParsedArgs} argsMap
  */
-function getToken(token) {
-    try {
-        return token && /.*(\.json|\.lic)/.test(token) ? JSON.parse(readFileSync(token, "utf8")).token : token;
-    } catch (error) {
-        console.error(error);
-        return "";
-    }
-}
-async function run() {
-    try {
-        const token = getToken(args.token);
-        if (!token) {
-            return Promise.reject(new Error("Token required"))
-        }
-        console.log("Token: '%s'", token || "?");
-        if (!args.host) {
-            return Promise.reject("Host required");
-        }
-        let url;
-        switch (args.mode) {
-            case "params": {
-                url = `${args.host}/${token}`;
-                break
-            }
-            case "headers": {
-                url = `${args.host}`;
-                break
-            }
-            case "query": {
-                url = `${args.host}?token=${token}`;
-                break;
-            }
-            default: {
-                url = `${args.host}?token=${token}`;
-            }
-        }
-        
-        const r = await fetch(url, {
-            method: "GET",
-            headers: args.mode === "headers" && { "Token": token } || undefined
-        });
-        console.log("url: '%s'", url);
-        const text = await r.text();
-        return `OK: ${r.ok}, Message: ${text || r.statusText || "?"}`
-    } catch (error) {
-        return Promise.reject(error);
-    }
-}
+export default async function(argsMap) {
+  const isFile = /.*(\.json|\.lic)/.test(argsMap.token);
 
-run().then(x => {
-    console.log("Result: %s", x);
-}).catch(error => {
+  /**
+   *
+   * @param {string} token
+   * @return {*}
+   */
+  function tryDecode(token) {
+    try {
+      const decoded = jwt.decode(token);
+      console.log("decoded: ", decoded);
+      return decoded;
+    } catch (error) {
+      console.error(error);
+      return {};
+    }
+  }
+
+  /**
+   * @param {string} filePath
+   * @return {{ token: string|undefined, validator: string|undefined}}
+   */
+  function getToken(filePath) {
+    const lic = JSON.parse(readFileSync(filePath, "utf8"));
+    console.log("lic: ", lic);
+    const { validator } = tryDecode(lic.token);
+    const { token } = lic;
+    return {
+      token,
+      validator,
+    };
+  }
+
+  try {
+    
+    const { token, validator } = isFile
+      ? getToken(argsMap.token)
+      : { token: argsMap.token, validator: undefined };
+
+    if (!token) {
+      return Promise.reject(new Error("Token required"));
+    }
+
+    console.log("Token: '%s'", token || "?");
+    const host = validator || argsMap.host;
+    if (!host) {
+      return Promise.reject("Host required");
+    }
+
+    let url;
+    switch (argsMap.mode) {
+      case "params": {
+        url = `${host}/${token}`;
+        break;
+      }
+      case "headers": {
+        url = `${host}`;
+        break;
+      }
+      case "query": {
+        url = `${host}?token=${token}`;
+        break;
+      }
+      default: {
+        url = `${host}?token=${token}`;
+      }
+    }
+
+    const r = await fetch(url, {
+      method: "GET",
+      headers: (argsMap.mode === "headers" && { Token: token }) || undefined,
+    });
+
+    console.log("url: '%s'", url);
+    const text = await r.text();
+
+    return `OK: ${r.ok}, Message: ${text || r.statusText || "?"}`;
+  } catch (error) {
     console.error(error);
-    process.exit(-1);
-});
+    return Promise.reject(error);
+  }
+}
